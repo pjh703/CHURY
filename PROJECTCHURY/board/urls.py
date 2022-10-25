7bb69df55d94ab7322651a3b454bd2e7ef6e88b0
박정현 <116129565+pjh703@users.noreply.github.com>
2022-10-25 09:36:20 +0900

    수정
    board 앱 urls.py 파일 추가

---
+++ PROJECTCHURY/board/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+
+app_name = 'board'
+urlpatterns = [
+    path('home/', views.home, name='home'),
+
+]
