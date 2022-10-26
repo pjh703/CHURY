45d3a5d688ced1aaffb648d6290589e4e23523d3
trulyeven <113951017+trulyeven@users.noreply.github.com>
2022-10-26 11:44:45 +0900

    ㅁ
    ㅁ

--- PROJECTCHURY/user/decorator.py
+++ PROJECTCHURY/user/decorator.py
@@ -7,13 +7,13 @@ from django.http import HttpResponse
 
 
 # 로그인 확인
-def login_message_required(function):
-    def wrap(request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            messages.info(request, "로그인한 사용자만 이용할 수 있습니다.")
-            return redirect(settings.LOGIN_URL)
-        return function(request, *args, **kwargs)
-    return wrap
+# def login_message_required(function):
+#     def wrap(request, *args, **kwargs):
+#         if not request.user.is_authenticated:
+#             messages.info(request, "로그인한 사용자만 이용할 수 있습니다.")
+#             return redirect(settings.LOGIN_URL)
+#         return function(request, *args, **kwargs)
+#     return wrap
 
 
 # 관리자 권한 확인
@@ -27,10 +27,10 @@ def admin_required(function):
 
 
 # 비로그인 확인
-def logout_message_required(function):
-    def wrap(request, *args, **kwargs):
-        if request.user.is_authenticated:
-            messages.info(request, "접속중인 사용자입니다.")
-            return redirect('/users/main/')
-        return function(request, *args, **kwargs)
-    return wrap
+# def logout_message_required(function):
+#     def wrap(request, *args, **kwargs):
+#         if request.user.is_authenticated:
+#             messages.info(request, "접속중인 사용자입니다.")
+#             return redirect('/users/main/')
+#         return function(request, *args, **kwargs)
+#     return wrap
