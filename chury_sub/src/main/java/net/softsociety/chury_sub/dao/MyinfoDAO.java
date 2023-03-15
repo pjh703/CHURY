00957a1e1f09ffe9fd9bb165ea0ed11dbc1ff206
trulyeven <113951017+trulyeven@users.noreply.github.com>
2023-03-15 18:34:16 +0900

    chury_sub 삭제
    chury_sub 삭제

--- chury_sub/src/main/java/net/softsociety/chury_sub/dao/MyinfoDAO.java
+++
@@ -1,9 +0,0 @@
-package net.softsociety.chury_sub.dao;
-
-import net.softsociety.chury_sub.domain.Myinfo;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface MyinfoDAO {
-    public Myinfo selectMyinfo(int id);
-}
--- chury_sub/src/main/java/net/softsociety/chury_sub/service/MyinfoService.java
+++
@@ -1,7 +0,0 @@
-package net.softsociety.chury_sub.service;
-
-import net.softsociety.chury_sub.domain.Myinfo;
-
-public interface MyinfoService {
-    public Myinfo read(int id);
-}
